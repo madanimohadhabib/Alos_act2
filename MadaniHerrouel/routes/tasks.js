a90f07a3192e8b4b05a22b8910085e10a1493b3c
madanimohadhabib <madanimohamed.habib@gmail.com>
2022-04-06 00:45:39 +0200

    Created the project two express js structure

---
+++ MadaniHerrouel/routes/tasks.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+const utils = require('../utils/task-schema.js');
+const tasks = require('../db.json')
+
+
+// GET
+router.get("/tasks" , (request, response) => {
+    response.send(tasks);
+});
+
+// GET (BY ID)
+router.get("/tasks/:id" , (request, response) => {
+    const taskId = request.params.id;
+    const task = tasks.find(task => task.id === parseInt(taskId));
+    if(!task) return response.status(404).send("The task with the provided ID does not exist.");
+    response.send(task);
+});
+
+// POST
+router.post("/tasks", (request, response) => {
+    const { error } = utils.validateTask(request.body);
+
+    if(error) return response.status(400).send("The name should be at least 3 chars long!")
+
+    const task = {
+        id: tasks.length + 1,
+        name: request.body.name,
+        surname: request.body.surname,
+        gender: request.body.gender,
+        age: request.body.age,
+        address: request.body.address,
+        email: request.body.email,
+        phone: request.body.phone,
+        speciality: request.body.speciality
+    };
+
+    tasks.push(task);
+    response.status(201).send(task);
+});
+
+
+
+//DELETE
+router.delete("/tasks/:id", (request, response) => {
+    const taskId = request.params.id;
+    const task = tasks.find(task => task.id === parseInt(taskId));
+    if(!task) return response.status(404).send("The task with the provided ID does not exist.");
+
+    const index = tasks.indexOf(task);
+    tasks.splice(index, 1);
+    response.send(task);
+});
+
+
+module.exports = router;
